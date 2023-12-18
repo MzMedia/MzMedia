f02e6dd0651a4792e90d958fbbc698b62db0e2d3
MzMedia <mzmedia2023@126.com>
2023-12-18 11:53:09 +0800

    1.代码修改

---
+++ mz-iot/mz-iot-components/mz-iot-network/src/main/java/com/mz/network/sip/VertxSipServer.java
@@ -0,0 +1,13 @@
+package com.mz.network.sip;
+
+import io.vertx.core.AbstractVerticle;
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class VertxSipServer extends AbstractVerticle {
+
+    @Override
+    public void start() throws Exception {
+
+    }
+
+}
---
+++ mz-iot/mz-iot-components/mz-iot-network/src/main/java/com/mz/network/tr069/VertxTr069Server.java
@@ -0,0 +1,12 @@
+package com.mz.network.tr069;
+
+import io.vertx.core.AbstractVerticle;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class VertxTr069Server extends AbstractVerticle {
+    @Override
+    public void start() throws Exception {
+
+    }
+}
